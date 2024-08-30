cb9c9832ebbf663d95b29cb28aba571a91b4fbd4
Ruslan Igorevich <r.borodavkin@eteron.ru>
2024-08-30 20:57:10 +0300

    added MainNavigation component

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,10 @@
+import MainNavigation from "@/components/shared/navigations/MainNavigation/MainNavigation";
+
 const Index = () => {
     return (
         <div>
-            Главная страница
+            <MainNavigation />
+            <h1>Главная страница</h1>
         </div>
     );
 }
